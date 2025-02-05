0b73539da26cadf7f33d360e8a28251d6a4ee02e
Akash <akash.bhuiyan@gmail.com>
2025-02-05 14:02:07 +0100

    Add Spring Cloud Stream support with RabbitMQ for messaging
    - Added `spring-cloud-stream` and `spring-cloud-stream-binder-rabbit` dependencies
    - Implemented `MessageFunctions` with `email` and `sms` functions for event-driven communication
    - Logging added to track message processing

---
+++ message/src/main/java/com/akash/message/dto/AccountsMsgDto.java
@@ -0,0 +1,9 @@
+package com.akash.message.dto;
+
+/**
+ * Author: akash
+ * Date: 4/2/25
+ */
+
+public record AccountsMsgDto(Long accountNumber, String name, String email, String mobileNumber) {
+}
---
+++ message/src/main/java/com/akash/message/functions/MessageFunctions.java
@@ -0,0 +1,36 @@
+package com.akash.message.functions;
+
+import com.akash.message.dto.AccountsMsgDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.function.Function;
+
+/**
+ * Author: akash
+ * Date: 4/2/25
+ */
+
+@Configuration
+public class MessageFunctions {
+
+    private static final Logger log = LoggerFactory.getLogger(MessageFunctions.class);
+
+    @Bean
+    public Function<AccountsMsgDto, AccountsMsgDto> email() {
+        return accountsMsgDto -> {
+            log.info("Sending email with the details : {}", accountsMsgDto.toString());
+            return accountsMsgDto;
+        };
+    }
+
+    @Bean
+    public Function<AccountsMsgDto,Long> sms() {
+        return accountsMsgDto -> {
+            log.info("Sending sms with the details : {}", accountsMsgDto.toString());
+            return accountsMsgDto.accountNumber();
+        };
+    }
+}
